7c8d751aa0e04751cc273772989944644e8366db
Kyle Aoki <kyle@Kyles-MacBook-Pro.local>
2022-05-01 16:01:14 -0500

    a

--- pw.go
+++ pw.go
@@ -9,76 +9,75 @@ import (
 	"time"
 )
 
-const lower_case_letters = "abcdefghijklmnopqrstuvwxyz"
-const upper_case_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-const numbers = "1234567890"
-const char_pool = lower_case_letters + upper_case_letters + numbers
+type Pool string
+
+const lowercasePool Pool = "abcdefghijklmnopqrstuvwxyz"
+const uppercasePool Pool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const numberPool Pool = "1234567890"
+const runePool Pool = lowercasePool + uppercasePool + numberPool
+
+type Password string
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
-	pwLength := obtainLengthFromArgs()
-	validatePwLength(pwLength)
 
-	for i := 0; i < 3000; i++ {
-		pw := generatePassword(pwLength)
-		if containsAll(pw) {
-			fmt.Println(pw)
-			os.Exit(0)
-		}
+	pwLength := obtainPasswordLengthFromArgs()
+	var pw Password
+
+	for !pw.hasElementsFromAllPools() {
+		pw = generatePassword(pwLength)
 	}
-	log.Fatal("Failed to generate password.")
+	
+	fmt.Println(pw)
 }
 
-func containsAll(pw string) bool {
-	return (contains(numbers, pw) &&
-		contains(upper_case_letters, pw) &&
-		contains(lower_case_letters, pw))
-}
-
-func isMember(pool string, char rune) bool {
-	for _, r := range pool {
-		if r == char {
-			return true
-		}
-	}
-	return false
-}
-
-func contains(pool string, pw string) bool {
-	for _, char := range pw {
-		if isMember(pool, char) {
-			return true
-		}
-	}
-	return false
-}
-
-func generatePassword(pwLength int) string {
-	pw := ""
+func generatePassword(pwLength int) Password {
+	var bytes []byte
 	for i := 0; i < pwLength; i++ {
-		randomInteger := rand.Intn(len(char_pool))
-		pw += string(char_pool[randomInteger])
+		i := rand.Intn(len(runePool))
+		bytes = append(bytes, runePool[i])
 	}
-	return pw
+	return Password(bytes)
 }
 
-func obtainLengthFromArgs() int {
+// true if pw has elements from every pool
+func (pw Password) hasElementsFromAllPools() bool {
+	return (pw.hasElementFrom(numberPool) &&
+		pw.hasElementFrom(uppercasePool) &&
+		pw.hasElementFrom(lowercasePool))
+}
+
+// returns true if pool contains given byte
+func (pool Pool) containsByte(b byte) bool {
+	for i := range pool {
+		if pool[i] == b {
+			return true
+		}
+	}
+	return false
+}
+
+// true if contains at least one element from specified pool
+func (pw Password) hasElementFrom(pool Pool) bool {
+	for i := range pw {
+		if pool.containsByte(pw[i]) {
+			return true
+		}
+	}
+	return false
+}
+
+// default password length is 20, unless first arg specifies an alternative length
+func obtainPasswordLengthFromArgs() int {
 	args := os.Args[1:]
 
 	if len(args) == 0 {
 		return 20
 	} else {
-		pwl, err := strconv.ParseInt(args[0], 10, 32)
+		pwLen, err := strconv.ParseInt(args[0], 10, 32)
 		if err != nil {
 			log.Fatal("First argument must be an integer.")
 		}
-
-		return int(pwl)
-	}
-}
-
-func validatePwLength(pwLength int) {
-	if pwLength < 4 {
-		log.Fatal("Password too short. Minumum 4 characters long.")
+		return int(pwLen)
 	}
 }
